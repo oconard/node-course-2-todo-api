3c8cfca51d5788076b88cb035ccab813fac12345
Olivier Conard <conard@Oliviers-MBP.hitronhub.home>
2018-07-21 07:14:22 -0600

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,7 +21,15 @@ todo.save().then((doc) => {
 })
 })
 
-app.listen(3002, () => {
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+  res.status(400).send(e);
+  })
+})
+
+app.listen(3003, () => {
   console.log('Started on port 3002');
 })
 
