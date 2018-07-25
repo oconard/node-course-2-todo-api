ef775b18bfb7a784dc55c898414db86dd9274c59
Olivier Conard <conard@Oliviers-MBP.hitronhub.home>
2018-07-25 04:35:13 -0600

    Add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -21,12 +22,36 @@ todo.save().then((doc) => {
 })
 })
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({todos})
-  }, (e) => {
-  res.status(400).send(e);
-  })
+// app.get('/todos', (req, res) => {
+//   Todo.find().then((todos) => {
+//     res.send({todos})
+//   }, (e) => {
+//   res.status(400).send(e);
+//   })
+// })
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+   return res.status(404).send();
+   }
+   Todo.findById(id).then((todo) => {
+     if (!todo) {
+       return res.status(404).send();
+     }
+     res.send({todo});
+   }).catch((e)  => {
+     res.status(400).send();
+
+   })
+
+  //404 - send back empty Send
+
+  //query database findById
+   //if todo - send it back
+   //if no tod -send back 404 empty body
+  //error
+    //400 - and send body back
 })
 
 app.listen(3003, () => {
